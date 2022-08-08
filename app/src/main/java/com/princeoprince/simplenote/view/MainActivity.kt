4d708b774316809d72c4531e21322ebdb29b9449
Prince Odia <princeodia@gmail.com>
2022-08-08 04:28:43 +0100

    Update build

--- app/src/main/java/com/princeoprince/simplenote/view/MainActivity.kt
+++ app/src/main/java/com/princeoprince/simplenote/view/MainActivity.kt
@@ -2,16 +2,18 @@ package com.princeoprince.simplenote.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.princeoprince.simplenote.R
+import com.princeoprince.simplenote.databinding.ActivityMainBinding
 import com.princeoprince.simplenote.controller.InternalFileRepository
 import com.princeoprince.simplenote.controller.NoteRepository
 
 class MainActivity : AppCompatActivity() {
 
     private val repo: NoteRepository by lazy { InternalFileRepository(this) }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
